369d98369e15c215e56a0be26cbcd5de1230fe05
huydo <dohuy171@gmail.com>
2025-07-21 18:45:27 +0700

    add ssl certification

--- scrape-email-fe/vite.config.ts
+++ scrape-email-fe/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import fs from "fs";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,6 +16,11 @@ export default defineConfig({
       allowedHeaders: ["Content-Type"], // Example headers
       credentials: true, // Example credentials setting
     },
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/cert.pem")),
+    },
+    host: "0.0.0.0",
     port: 8000, // Change this to your desired port
     open: true, // Automatically open the browser
   },
