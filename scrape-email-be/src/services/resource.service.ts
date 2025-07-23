aca2eec78c32e372b469b31d886cb45781ef7509
huydo <dohuy171@gmail.com>
2025-07-23 17:09:26 +0700

    debug

--- scrape-email-be/src/services/resource.service.ts
+++ scrape-email-be/src/services/resource.service.ts
@@ -123,26 +123,21 @@ export class ResourceService implements IResourceService {
             //
             // wait 2 seconds each time
             //await new Promise((res) => setTimeout(res, 2000))
-
-            const sendEmailPromises = chunk(
-                emails.map((email) =>
-                    this.SendEmail(
-                        accessToken || '',
-                        email,
-                        subject,
-                        body,
-                        attachedFiles
-                    )
-                ),
-                3
-            ) // each batch length is 3
-
+            const sendEmailPromises = emails.map((email) =>
+                this.SendEmail(
+                    accessToken || '',
+                    email,
+                    subject,
+                    body,
+                    attachedFiles
+                )
+            )
             // Wait for all emails to be sent
             let results: { email: string; success: boolean }[] = []
             for (const sendEmail of sendEmailPromises) {
                 // Wait for all emails to be sent
-                const chunk = await Promise.all(sendEmail)
-                results.push(...chunk)
+                const email = await sendEmail
+                results.push(email)
                 await await new Promise((res) => setTimeout(res, 6000))
             }
 
